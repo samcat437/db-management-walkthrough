2c33fcc56d08dfe14ae88c9582b1961000ab53f9
samcat437 <sdrees12@gmail.com>
2022-06-28 14:57:45 +0000

    adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
   });
+
+  selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
 
  
