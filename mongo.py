6e976a8d3846b81f7e6cb66e9a18941b7ea1b10c
samcat437 <sdrees12@gmail.com>
2022-06-30 15:38:43 +0000

    mongo db cli introduction

---
+++ mongo.py
@@ -0,0 +1,40 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try: 
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e: 
+        print("Could not connect to MongoDB: %s") % e
+
+    
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+""" Insert a single document """
+new_doc = {
+    "first": "douglas",
+    "last": "adams",
+    "dob": "11/03/1952",
+    "gender": "m",
+    "hair_color": "grey",
+    "occupation": "writer",
+    "nationality": "british"
+}
+
+coll.insert_one(new_doc)
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
